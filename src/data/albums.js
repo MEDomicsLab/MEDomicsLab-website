6548e67e34f50e1eba57e9939902dab5484e3543
Provost Simon <simon1.provost@epitech.eu>
2026-09-02 14:57:09 -0400

    feat(data): add content schemas and structured site data

---
+++ src/data/albums.js
@@ -0,0 +1,26 @@
+export const albumImages = {
+  "2022-08-05-mini-putt": [
+    { src: "/images/albums/2022-08-05-mini-putt/mini-putt-1.png", alt: "Mini putt outing 1" },
+    { src: "/images/albums/2022-08-05-mini-putt/mini-putt-2.png", alt: "Mini putt outing 2" },
+    { src: "/images/albums/2022-08-05-mini-putt/mini-putt-3.png", alt: "Mini putt outing 3" },
+    { src: "/images/albums/2022-08-05-mini-putt/mini-putt-4.png", alt: "Mini putt outing 4" },
+    { src: "/images/albums/2022-08-05-mini-putt/mini-putt-5.png", alt: "Mini putt outing 5" },
+    { src: "/images/albums/2022-08-05-mini-putt/mini-putt-6.png", alt: "Mini putt outing 6" },
+    { src: "/images/albums/2022-08-05-mini-putt/mini-putt-7.png", alt: "Mini putt outing 7" },
+    { src: "/images/albums/2022-08-05-mini-putt/mini-putt-8.png", alt: "Mini putt outing 8" },
+    { src: "/images/albums/2022-08-05-mini-putt/mini-putt-9.png", alt: "Mini putt outing 9" },
+  ],
+  "2022-08-12-siboire": [
+    { src: "/images/albums/2022-08-12-siboire/siboire-1.png", alt: "Siboire gathering" },
+  ],
+  "2022-10-20-cirius": [
+    {
+      src: "/images/albums/2022-10-20-cirius/cirius-all.png",
+      alt: "CIRIUS Science Day group photo",
+    },
+    {
+      src: "/images/albums/2022-10-20-cirius/cirius-hakima.png",
+      alt: "CIRIUS Science Day award photo",
+    },
+  ],
+};
